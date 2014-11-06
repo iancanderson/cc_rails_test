d621d99f77496a77296749e3831971ff2bb246e4
Ian C. Anderson <ian@redheadedlabs.com>
2014-11-06 12:00:37 -0500

    fewer tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Post, :type => :model do
   describe "body_size" do
-    it "works" do
-      expect(Post.new(body: "bac").body_size).to eq(3)
-    end
+    # it "works" do
+    #   expect(Post.new(body: "bac").body_size).to eq(3)
+    # end
   end
 end
